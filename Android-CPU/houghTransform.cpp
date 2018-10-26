3ef46468d0fa422152b8b6c70526f419e758490d
jbgoldst5678 <jbgoldst@andrew.cmu.edu>
2018-10-26 19:30:26 -0400

    Add files via upload

--- Android-CPU/houghTransform.cpp
+++ Android-CPU/houghTransform.cpp
@@ -1,10 +1,11 @@
-# include <cstdlib>
-# include <iostream>
-# include <stdint.h>
-# include <stdio.h>
-# include <cmath>
-# include "jpeglib.h"
+#include <cstdlib>
+#include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+#include <cmath>
+#include "fpmath/include/fpml/fixed_point.h"
 using namespace std;
+using namespace fpml;
 
 #define NUM_CHANNELS 3
 #define MAT_LEN 256
@@ -14,19 +15,22 @@ using namespace std;
 #define B_SHIFT 2
 #define PI 3.14159265
 #define MAX_PIXEL_VALUE 255
+#define FIXED_PT_PRECISION 16
 
-void rgb2gray( unsigned char ***matrix, double **result );
-void conv2D( double **matrix, double kernel[FILT_LEN][FILT_LEN], double **result );
-void blur( double **matrix, double **result );
-void gradient( double **matrix, double **grad_x, double **grad_y );
-void magnitude( double **x, double **y, double **mag );
-void angle( double **x, double **y, int ** angle );
-void threshold( double **matrix, double **result, double threshold);
+typedef fixed_point<int,FIXED_PT_PRECISION> fp;
+
+void rgb2gray( unsigned char matrix[MAT_LEN][MAT_LEN][NUM_CHANNELS], fp result[MAT_LEN][MAT_LEN] );
+void conv2D( fp matrix[MAT_LEN][MAT_LEN], fp kernel[FILT_LEN][FILT_LEN], fp result[MAT_LEN][MAT_LEN] );
+void blur( fp matrix[MAT_LEN][MAT_LEN], fp result[MAT_LEN][MAT_LEN] );
+void gradient( fp matrix[MAT_LEN][MAT_LEN], fp grad_x[MAT_LEN][MAT_LEN], fp grad_y[MAT_LEN][MAT_LEN] );
+void magnitude( fp x[MAT_LEN][MAT_LEN], fp y[MAT_LEN][MAT_LEN], fp mag[MAT_LEN][MAT_LEN] );
+void angle( fp x[MAT_LEN][MAT_LEN], fp y[MAT_LEN][MAT_LEN], int angle[MAT_LEN][MAT_LEN] );
+void threshold( fp matrix[MAT_LEN][MAT_LEN], fp result[MAT_LEN][MAT_LEN], fp threshold);
 int main ( int argc, char *argv[] );
 
 
 // converts RGB matrix into normalized grayscale matrix
-void rgb2gray( unsigned char ***matrix, double **result )
+void rgb2gray( unsigned char matrix[MAT_LEN][MAT_LEN][NUM_CHANNELS], fp result[MAT_LEN][MAT_LEN] )
 {
     int i,j,k;
     for(i = 0; i < MAT_LEN; i++)
@@ -40,9 +44,9 @@ void rgb2gray( unsigned char ***matrix, double **result )
 }
 
 // performs 2D convolution with an input matrix and kernel
-void conv2D( double **matrix, double kernel[FILT_LEN][FILT_LEN], double **result )
+void conv2D( fp matrix[MAT_LEN][MAT_LEN], fp kernel[FILT_LEN][FILT_LEN], fp result[MAT_LEN][MAT_LEN] )
 {
-    double cur_pixel;
+    fp cur_pixel;
     int row,col,i,j,m,n;
     for(i = 0; i < MAT_LEN; i++)
     {
@@ -67,9 +71,9 @@ void conv2D( double **matrix, double kernel[FILT_LEN][FILT_LEN], double **result
 }
 
 // performs Gaussian blurring on the image
-void blur( double **matrix, double **result )
+void blur( fp matrix[MAT_LEN][MAT_LEN], fp result[MAT_LEN][MAT_LEN] )
 {
-    double gaussian_kernel[FILT_LEN][FILT_LEN] =
+    fp gaussian_kernel[FILT_LEN][FILT_LEN] =
     {
         {1.0/16.0, 2.0/16.0, 1.0/16.0},
         {2.0/16.0, 4.0/16.0, 2.0/16.0},
@@ -80,16 +84,16 @@ void blur( double **matrix, double **result )
 }
 
 // computes the gradients in the x and y directions
-void gradient( double **matrix, double **grad_x, double **grad_y )
+void gradient( fp matrix[MAT_LEN][MAT_LEN], fp grad_x[MAT_LEN][MAT_LEN], fp grad_y[MAT_LEN][MAT_LEN] )
 {
-    double sobel_x[FILT_LEN][FILT_LEN] =
+    fp sobel_x[FILT_LEN][FILT_LEN] =
     {
         {-1, 0, 1},
         {-2, 0, 2},
         {-1, 0, 1}
     };
 
-    double sobel_y[FILT_LEN][FILT_LEN] =
+    fp sobel_y[FILT_LEN][FILT_LEN] =
     {
         {1,  2,  1},
         {0,  0,  0},
@@ -101,34 +105,34 @@ void gradient( double **matrix, double **grad_x, double **grad_y )
 }
 
 // computes the magnitude of the gradient at every pixel
-void magnitude( double **x, double **y, double **mag )
+void magnitude( fp x[MAT_LEN][MAT_LEN], fp y[MAT_LEN][MAT_LEN], fp mag[MAT_LEN][MAT_LEN] )
 {
     int i,j;
     for(i = 0; i < MAT_LEN; i++)
     {
         for(j = 0; j < MAT_LEN; j++)
         {
-            mag[i][j] = sqrt(pow(x[i][j],2) + pow(y[i][j],2));
+            mag[i][j] = sqrt((x[i][j]*x[i][j]) + (y[i][j]*y[i][j]));
         }
     }
 }
 
 // computes the angle of the gradient at every pixel, rounds to nearest 45 degrees
-void angle( double **x, double **y, int **angle )
+void angle( fp x[MAT_LEN][MAT_LEN], fp y[MAT_LEN][MAT_LEN], int angle[MAT_LEN][MAT_LEN] )
 {
     int i,j;
     for(i = 0; i < MAT_LEN; i++)
     {
         for(j = 0; j < MAT_LEN; j++)
         {
-            double cur_angle = atan2(y[i][j], x[i][j]) * 180 / PI;
-            angle[i][j] = (int)round(cur_angle / 45);
+            fp cur_angle = fp(atan2(((int)y[i][j])<<16, ((int)x[i][j])<<16)) * fp(180 / PI);
+            angle[i][j] = (int)round(cur_angle / fp(45));
         }
     }
 }
 
 // thresholds all pixel values to 0 or 1
-void threshold( double **matrix, double **result, double threshold )
+void threshold( fp matrix[MAT_LEN][MAT_LEN], fp result[MAT_LEN][MAT_LEN], fp threshold )
 {
     int i,j;
     for(i = 0; i < MAT_LEN; i++)
@@ -142,23 +146,22 @@ void threshold( double **matrix, double **result, double threshold )
 
 int main ( int argc, char *argv[] )
 {
-    cout.precision(8);
-
-    unsigned char ***matrix;
-    matrix = new unsigned char **[MAT_LEN];
+    unsigned char matrix[MAT_LEN][MAT_LEN][NUM_CHANNELS];
     for(int i = 0; i < MAT_LEN; i++)
     {
-        matrix[i] = new unsigned char *[MAT_LEN];
-        for(int j = 0; j < NUM_CHANNELS; j++)
+        for(int j = 0; j < MAT_LEN; j++)
         {
-            matrix[i][j] = new unsigned char [NUM_CHANNELS];
+            for(int k = 0; k < NUM_CHANNELS; k++)
+            {
+                matrix[i][j][k] = 0;
+            }
         }
     }
 
-    double **gray = {0};
-    double **blurred = {0};
-    double **grad_x = {0};
-    double **grad_y = {0};
+    fp gray[MAT_LEN][MAT_LEN] = {0};
+    fp blurred[MAT_LEN][MAT_LEN] = {0};
+    fp grad_x[MAT_LEN][MAT_LEN] = {0};
+    fp grad_y[MAT_LEN][MAT_LEN] = {0};
 
 
     rgb2gray(matrix, gray);
